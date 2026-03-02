559c5a3dc1e053964a016d7bf14a9447788cab5e
Michael Stager <61891711+MichaelStager@users.noreply.github.com>
2026-03-01 20:42:27 -0800

    added some scenes

---
+++ src/prefabs/doorWay.js
@@ -0,0 +1,30 @@
+class DoorWay extends Phaser.GameObjects.Image
+{
+    constructor(scene,x,y,key,tranScene)
+    {
+        super(scene,x,y,key)
+        this.setOrigin(0.5,0.5)
+        scene.add.existing(this)
+        this.setInteractive({
+            pixelPerfect: true,
+            alphaTolerance: 1  
+        })
+      
+         // hover feedback
+        this.on('pointerover', () => {
+            this.setScale(1.10)
+            this.scene.scene.get('uiScene').cursorImage.setTexture('backArrow');
+            //perhaps play a sound
+        });
+        this.on('pointerout',  () => {
+            this.setScale(1)
+            this.scene.scene.get('uiScene').cursorImage.setTexture('testImg');
+        });
+
+        // click behavior, get the text from the ui scene and set it to description string.
+        this.on('pointerdown', () => {
+            this.scene.scene.start(tranScene)
+        })
+    
+    }
+}
--- src/scenes/TowerDoor.js
+++ src/scenes/TowerDoor.js
@@ -1,12 +1,8 @@
-//pointer events that we will prob use
-//1. 'pointerdown' for clicking
-//2. 'pointerover' for hoving
-
-class Play extends Phaser.Scene
+class TowerDoor extends Phaser.Scene
 {
     constructor()
     {
-        super("playScene")
+        super("towerDoorScene")
     }
 
     create()
@@ -15,12 +11,8 @@ class Play extends Phaser.Scene
        this.sceneBG = this.add.image(width/2,playHeight/2,'testBG')
         //Object examples of making a clickable object and FindableObject
         this.testImage2 = new ClickableObject(this,100,250,'testImg',"A Brave wariors remains...\n I wonder why he left his\n weapon? Maybe he became \n a monk?")
-        this.toTowerDoor = new DoorWay(this,400,250,'testImg','introScene' )
-        if(!this.scene.manager.getScene('uiScene').itemsFound[0])
-        {
-            this.testImage3 = new FindableObject(this,850,250,'testImg','TowerDoor',0)
-        }
-       
+        this.toTowerDoor = new DoorWay(this,400,250,'testImg','stairWellScene' )
+        
       //this needs to be in a scene we dont go back to , like between the menu and first play scene, this will keep relaunching a new ui  
      // this.ui =  this.scene.launch('uiScene')
     }
