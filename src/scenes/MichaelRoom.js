68fcfb60e504dfb62325fb20d363a9e491a64f4b
Michael Stager <61891711+MichaelStager@users.noreply.github.com>
2026-03-05 14:01:14 -0800

    added michaelscomputer

--- src/scenes/MichaelRoom.js
+++ src/scenes/MichaelRoom.js
@@ -8,7 +8,7 @@ class MichaelRoom extends Phaser.Scene {
         this.sceneBG = this.add.image(width / 2, playHeight / 2, 'michaelRoomBG')
         //Object examples of making a clickable object and FindableObject
         this.michaelPillow = new ClickableObject(this, 246, 216, 'michaelPillow', "A pile of blue pillows...\n Something feels innoccent \n in the color.")
-        this.michaelComputer = new ClickableObject(this, 776, 147, 'michaelComputer', "A pile of blue pillows...\n Something feels innoccent \n in the color.")
+        this.michaelComputer = new FindableObject(this, 776, 147, 'michaelComputer', 'computerCloseUpScene')
         this.michaelFurnace = new ClickableObject(this, -16, -44, 'michaelFurnace', "A pile of blue pillows...\n Something feels innoccent \n in the color.")
 
         //Important Items
