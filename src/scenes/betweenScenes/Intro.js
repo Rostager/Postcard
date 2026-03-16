9c52fa6dee2551d31f5a88a00b5e40b69c9cb4da
Michael Stager <61891711+MichaelStager@users.noreply.github.com>
2026-03-16 12:38:19 -0700

    important items now tint black to cleartint, and ui text is now green with stroke

--- src/scenes/betweenScenes/Intro.js
+++ src/scenes/betweenScenes/Intro.js
@@ -34,6 +34,10 @@ class Intro extends Phaser.Scene
             alphaTolerance: 1
         })
         this.postcard = this.add.image(width/2,height/2,'letterPortalFront').setAlpha(0)
+
+        //text
+        this.add.text("A Letter has arrived")
+
         //I lowkey need to explain the envelope, the way its setup 
         this.envelope.on('pointerover',()=>{
             this.envelope.setTint(0xe7e7e7)
