8200757da76a3b3df7113a874085622b679941c1
Robert Stager <queenbee.stager@gmail.com>
2026-03-16 21:01:13 -0700

    Commit before merging to main

--- src/scenes/ElevatorScenes/ElevatorRoomPaint.js
+++ src/scenes/ElevatorScenes/ElevatorRoomPaint.js
@@ -14,16 +14,16 @@ class ElevatorRoomPainted extends Phaser.Scene {
         this.elevatorDoor = new DoorWay(this, 480, 1000, 'elevatorClosed', 'michaelRoomScene', 'elevatorOpen').setOrigin(0.5,0.5)
         this.sceneBGBottom = this.add.rectangle(480,320,960,80,0xF0EEED).setOrigin(0.5,0.5) 
 
-        this.canvas1 = new ClickableObject(this,160,160,'canvas1', 'An Empty Canvas','brushSFX').setOrigin(0.5,0.5).setFrame(0)
+        this.canvas1 = new ClickableObject(this,160,160,'canvas1', 'Art representing self','brushSFX').setOrigin(0.5,0.5).setFrame(0)
         this.gear1 = this.add.image(160,1000,'gear4').setOrigin(0.5,0.5).setScale(0.5)
 
-        this.canvas2 = new ClickableObject(this,320,160,'canvas2', 'An Empty Canvas','brushSFX').setOrigin(0.5,0.5).setFrame(0)
+        this.canvas2 = new ClickableObject(this,320,160,'canvas2', 'Art representing meaning','brushSFX').setOrigin(0.5,0.5).setFrame(0)
         this.gear2 = this.add.image(320,1000,'gear1').setOrigin(0.5,0.5).setScale(0.5)
 
-        this.canvas3 = new ClickableObject(this,640,160,'canvas3', 'An Empty Canvas','brushSFX').setOrigin(0.5,0.5).setFrame(0)
+        this.canvas3 = new ClickableObject(this,640,160,'canvas3', 'Art representing \nexperimentation','brushSFX').setOrigin(0.5,0.5).setFrame(0)
         this.gear3 = this.add.image(640,1000,'gear2').setOrigin(0.5,0.5).setScale(0.5)
 
-        this.canvas4 = new ClickableObject(this,800,160,'canvas4', 'An Empty Canvas','brushSFX').setOrigin(0.5,0.5).setFrame(0)
+        this.canvas4 = new ClickableObject(this,800,160,'canvas4', 'Art representing love','brushSFX').setOrigin(0.5,0.5).setFrame(0)
         this.gear4 = this.add.image(800,1000,'gear3').setOrigin(0.5,0.5).setScale(0.5)
         //Logic to kill BGM on Scene Close
          this.events.once('shutdown', () => {
@@ -39,10 +39,11 @@ class ElevatorRoomPainted extends Phaser.Scene {
                 switch (this.canvas1Clicks) {
                     case 0:
                         //You must write what the next painting will be of in this description
-                        this.canvas1.changeDescription('Test.')
+                        this.canvas1.changeDescription('It\'s important to capture \nyour internal feelings')
                         this.canvas1.setFrame(1)
                         break
                     case 1:
+                        this.canvas1.changeDescription('SELF')
                         this.canvas1.setFrame(2)
                         break
                     case 2:
@@ -59,10 +60,11 @@ class ElevatorRoomPainted extends Phaser.Scene {
                 switch (this.canvas2Clicks) {
                     case 0:
                         //You must write what the next painting will be of in this description
-                        this.canvas2.changeDescription('Test.')
+                        this.canvas2.changeDescription('It\'s important to use your \nart to express what you \ncare about')
                         this.canvas2.setFrame(1)
                         break
                     case 1:
+                        this.canvas2.changeDescription('MEANING')
                         this.canvas2.setFrame(2)
                         break
                     case 2:
@@ -79,10 +81,11 @@ class ElevatorRoomPainted extends Phaser.Scene {
                 switch (this.canvas3Clicks) {
                     case 0:
                         //You must write what the next painting will be of in this description
-                        this.canvas3.changeDescription('Test.')
+                        this.canvas3.changeDescription('It\'s important to experiment \nand try new things, both \nin art and in life')
                         this.canvas3.setFrame(1)
                         break
                     case 1:
+                        this.canvas3.changeDescription('EXPERIMENTATION')
                         this.canvas3.setFrame(2)
                         break
                     case 2:
@@ -99,10 +102,11 @@ class ElevatorRoomPainted extends Phaser.Scene {
                 switch (this.canvas4Clicks) {
                     case 0:
                         //You must write what the next painting will be of in this description
-                        this.canvas4.changeDescription('Test.')
+                        this.canvas4.changeDescription('It\'s important to use the \nlove from others to \ninspire your art')
                         this.canvas4.setFrame(1)
                         break
                     case 1:
+                        this.canvas4.changeDescription('LOVE')
                         this.canvas4.setFrame(2)
                         break
                     case 2:
@@ -153,6 +157,7 @@ class ElevatorRoomPainted extends Phaser.Scene {
             this.tweens.add({
                 targets: this.robertSitting,
                 alpha:0,
+                x: 1000,
                 duration: 2000,
                 ease: 'Linear',
             })
